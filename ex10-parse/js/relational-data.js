a6ab0d95a0152392506c6e03a3de1825d210297b
fellyph <fellyph.cintra@gmail.com>
2015-09-22 00:24:32 -0300

    convert exemplo to object

--- ex10-parse/js/relational-data.js
+++ ex10-parse/js/relational-data.js
@@ -1,73 +1,75 @@
 Parse.initialize("2fCYJxTlUgudJlmCFKjYLkvaZCE2xukZoPef87GY", "xR6psxbG0H2KtwKAsCtvDsY6QHljJZCXlgJLxoMN");
 
+var app = {
+    Post: null,
+    Comment: null,
+    post: null,
+    commentForm: null,
+    posts: null,
+    postsResult: null,
 
-// Criando as variáveis que iremos trabalhar
-var Post = Parse.Object.extend("Post"),
-    Comment = Parse.Object.extend("Comment"),
-    post = new Post(),
-    commentForm,
-    posts,
-    postsResult;
+    init: function() {
+        //selecionando os elementos
+        app.commentForm  = document.querySelector("#comment-form");
+        app.commentForm.addEventListener("submit", app.sendComment);
+        app.posts = document.querySelector(".posts");
+
+        app.Post = Parse.Object.extend("Post");
+        app.Comment = Parse.Object.extend("Comment");
+        app.loadAllPosts();
+    },
+
+    loadAllPosts: function() {
+        //chamamos a função Query do Parse parar varer a nossa base
+        var query = new Parse.Query("Post");
+
+        //a função trata a query para sucesso ou erro de nossa consulta
+        query.find({
+            success: function(results){
+                app.showPosts(results);
+            },
+            error: function(error){
+                //tratamento para caso de erro
+                console.log("error", error);
+            }
+        });
+    },
+
+    showPosts: function(results) {
+        postsResult = results;
+        postsResult.forEach(function(post) {
+            app.createPost(post);
+        });
+    },
+
+    createPost: function(post) {
+        var htmlMarkup = "<article data-id='" + post.id + "'>" +
+                        "<h2>" + post.attributes.title + "</h2>" +
+                        "<p>" + post.attributes.content + "</p>" +
+                        "</article>";
+
+        app.posts.innerHTML += htmlMarkup;
+        app.commentForm.dataset.id = post.id;
+    },
+
+    sendComment: function(event) {
+        event.preventDefault();
+        var target = event.srcElement || event.target;
+        var comment = new app.Comment();
+        var post = new app.Post();
+        post.id = target.dataset.id;
+        comment.save({
+            name: target.name.value,
+            email: target.email.value,
+            message: target.message.value,
+            parentPost: post
+        }).then(function(result){
+            console.log(result);
+        })
+        console.log(event);
+    }
+};
 
 window.addEventListener("load", function() {
-    // selecionando o  form que irá enviar os comentários
-	commentForm  = document.querySelector("#comment-form");
-    posts = document.querySelector(".posts");
-	// adicionando o evento para acompanhar qualquer envio do formulário
-	commentForm.addEventListener("submit", sendComment);
-    loadAllPosts();
-})
-
-
-function loadAllPosts() {
-    //chamamos a função Query do Parse parar varer a nossa base
-    var query = new Parse.Query("Post");
-
-    //a função trata a query para sucesso ou erro de nossa consulta
-    query.find({
-        success: function(results){
-            showPosts(results);
-        },
-        error: function(error){
-            //tratamento para caso de erro
-            console.log("error", error);
-        }
-    });
-}
-
-function showPosts(results) {
-    postsResult = results;
-    postsResult.forEach(function(post) {
-        createPost(post);
-    });
-}
-
-function createPost(post) {
-    var htmlMarkup = "<article data-id='" + post.id + "'>" +
-                    "<h2>" + post.attributes.title + "</h2>" + 
-                    "<p>" + post.attributes.content + "</p>" +
-                    "</article>";
-    
-    posts.innerHTML += htmlMarkup;
-    commentForm.dataset.id = post.id;
-}
-
-//
-function sendComment(event) {
-	event.preventDefault();
-    var target = event.srcElement || event.target;
-    var comment = new Comment();
-    var post = new Post();
-    post.id = target.dataset.id;
-    comment.save({
-        name: target.name.value,
-        email: target.email.value,
-        message: target.message.value,
-        parentPost: post 
-    }).then(function(result){
-        console.log(result);
-    })
-    console.log(event);
-    //comment.save({});
-}
-
+   app.init()
+});
